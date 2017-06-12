9eaaf7a65b22dd0d0fbd3d05ab5e38966e1a27f9
Anthony McClay <anthony.mcclay@nielsen.com>
2017-06-12 02:13:40 -0400

    Update with working Django Code

--- gamesapi/gamesapi/urls.py
+++ gamesapi/gamesapi/urls.py
@@ -1,7 +1,13 @@
+"""
+Book: Building RESTful Python Web Services
+Chapter 1: Developing RESTful APIs with Django
+Author: Gaston C. Hillar - Twitter.com/gastonhillar
+Publisher: Packt Publishing Ltd. - http://www.packtpub.com
+"""
 """gamesapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +19,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include('games.urls')),
 ]
