9bab61bdce64a0c28d0ed3b42f80bf376f04a854
amcclay <anthony.mcclay@me.com>
2017-06-12 18:40:48 -0400

    Added pagination

--- gamesapi/gamesapi/settings.py
+++ gamesapi/gamesapi/settings.py
@@ -133,9 +133,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS':
+#     'rest_framework.pagination.LimitOffsetPagination',
+#     'PAGE_SIZE': 5
+# }
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
     'games.pagination.LimitOffsetPaginationWithMaxLimit',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        )
 }
 
